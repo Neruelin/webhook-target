15a2f4a5a1f5d78e1e0707ce0ce230bb4c31f16a
neruelin <neruelin@gmail.com>
2024-09-18 23:57:37 -0700

    update reset logic

--- src/main.js
+++ src/main.js
@@ -1,41 +1,65 @@
 var http = require("http");
-const { spawn } = require("node:child_process");
+const { spawn, exec } = require("node:child_process");
 const process = require("node:process");
 
-function startAppServer() {
-    let child = spawn('sh', ['./script.sh'], {detached: true});
-
-    let temp = {pid: child.pid};
-
-    child.stdout.on('data', (data) => {
-        console.log(`stdout (${temp.pid}): ${data}`);
-      });
-      
-      child.stderr.on('data', (data) => {
-        console.error(`stderr (${temp.pid}): ${data}`);
-      });
-      
-      child.on('close', (code) => {
-        console.log(` (${temp.pid}) child process exited with code ${code}`);
-      });
-      return child;
+async function getPid(port) {
+  return new Promise((resolve) => {
+    exec(`sudo netstat -tlnp | awk -F '  ' '/:${port} */ {split($NF,a,"/"); return a[1]}'`, (err, stdout, stderr) => {
+      resolve(stdout.trim().split('\n'));
+    });
+  });
 }
 
-const runAppServer = {process: startAppServer()}
+async function reset() {
+  let p3000 = await getPid(3000);
+  let p3001 = await getPid(3001);
+  let p3003 = await getPid(3003);
+  let p3004 = await getPid(3004);
+  try {
+    process.kill(parseInt(p3000));
+  } catch (err) {}
+  try {
+    process.kill(parseInt(p3001));
+  } catch (err) {}
+  try {
+    process.kill(parseInt(p3003));
+  } catch (err) {}
+  try {
+    process.kill(parseInt(p3004));
+  } catch (err) {}
+}
+
+function startAppServer() {
+  let child = spawn('sh', ['./script.sh'], { detached: true });
+
+  console.log("spawned " + child.pid);
+
+  let temp = { pid: child.pid };
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout (${temp.pid} : ${process.pid}): ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr (${temp.pid}): ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(` (${temp.pid}) child process exited with code ${code}`);
+  });
+}
 
 http.createServer(async (req, res) => {
 
-    res.writeHead(200);
-    res.end();
+  res.writeHead(200);
+  res.end();
 
-    if (runAppServer.process && runAppServer.process.killed != true) {
-        process.kill(-runAppServer.process.pid)
-    }
+  await reset();
 
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    runAppServer.process = startAppServer();
-    
+  startAppServer();
+
 }).listen(3002, '0.0.0.0', () => {
   console.log("started webhook server on http://0.0.0.0:3002");
 })
